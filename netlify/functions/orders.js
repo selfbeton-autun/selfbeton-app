9e277a1a985d46dfc09ae524245178cb496ec30b
selfbeton-autun <autun.selfbeton@gmail.com>
2026-03-14 08:44:59 +0100

    Update orders.js

--- netlify/functions/orders.js
+++ netlify/functions/orders.js
@@ -1,33 +1,38 @@
+javascriptconst https = require('https');
+
 exports.handler = async function(event) {
   const token = event.headers['x-token'] || '';
   const SHOP_ID = 339;
   
-  try {
-    const response = await fetch(
-      `https://beton2go.primarkets.fr/api/v1/om/orders?filter%5Border%5D=orderDate+DESC&page=&shopId=${SHOP_ID}&size=50`,
-      {
-        headers: {
-          'Authorization': 'Bearer ' + token,
-          'Accept': 'application/json',
-          'Current-Shop': String(SHOP_ID)
-        }
-      }
-    );
-    
-    const data = await response.json();
-    
-    return {
-      statusCode: 200,
+  const url = `https://beton2go.primarkets.fr/api/v1/om/orders?filter%5Border%5D=orderDate+DESC&page=&shopId=${SHOP_ID}&size=50`;
+
+  return new Promise((resolve) => {
+    const options = {
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+        'Authorization': 'Bearer ' + token,
+        'Accept': 'application/json',
+        'Current-Shop': String(SHOP_ID)
+      }
     };
-  } catch(err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    };
-  }
+
+    https.get(url, options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+          },
+          body: data
+        });
+      });
+    }).on('error', (err) => {
+      resolve({
+        statusCode: 500,
+        body: JSON.stringify({ error: err.message })
+      });
+    });
+  });
 };
