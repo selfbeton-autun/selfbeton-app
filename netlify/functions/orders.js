90e2f22d3b1595162821119b41b75c0cdc75ab7e
selfbeton-autun <autun.selfbeton@gmail.com>
2026-03-14 08:53:00 +0100

    Update orders.js

--- netlify/functions/orders.js
+++ netlify/functions/orders.js
@@ -1,17 +1,16 @@
 const https = require('https');
 
 exports.handler = async function(event) {
-  const token = event.headers['x-token'] || 'NO_TOKEN';
+  const token = event.headers['x-token'] || '';
   const SHOP_ID = 339;
-  
-  const url = `beton2go.primarkets.fr`;
   const path = `/api/v1/om/orders?filter%5Border%5D=orderDate+DESC&page=&shopId=${SHOP_ID}&size=50`;
 
   return new Promise((resolve) => {
     const options = {
-      hostname: url,
+      hostname: 'beton2go.primarkets.fr',
       path: path,
       method: 'GET',
+      rejectUnauthorized: false,
       headers: {
         'Authorization': 'Bearer ' + token,
         'Accept': 'application/json',
@@ -27,19 +26,15 @@ exports.handler = async function(event) {
         resolve({
           statusCode: 200,
           headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            status: res.statusCode,
-            headers: res.headers,
-            data: data.substring(0, 500)
-          })
+          body: data
         });
       });
     });
 
     req.on('error', (err) => {
       resolve({
-        statusCode: 200,
-        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
         body: JSON.stringify({ error: err.message, code: err.code })
       });
     });
