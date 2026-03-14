4bf82a2310843a9ad1843ed19f5600d0cd03cb5a
selfbeton-autun <autun.selfbeton@gmail.com>
2026-03-14 08:51:00 +0100

    Update orders.js

--- netlify/functions/orders.js
+++ netlify/functions/orders.js
@@ -1,45 +1,6 @@
 const https = require('https');
 
 exports.handler = async function(event) {
-  const token = event.headers['x-token'] || '';
-  const SHOP_ID = 339;
-  
-  const url = `https://beton2go.primarkets.fr/api/v1/om/orders?filter%5Border%5D=orderDate+DESC&page=&shopId=${SHOP_ID}&size=50`;
-
-  return new Promise((resolve) => {
-    const options = {
-      headers: {
-        'Authorization': 'Bearer ' + token,
-        'Accept': 'application/json',
-        'Current-Shop': String(SHOP_ID)
-      }
-    };
-
-    https.get(url, options, (res) => {
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
-          },
-          body: data
-        });
-      });
-    }).on('error', (err) => {
-      resolve({
-        statusCode: 500,
-        body: JSON.stringify({ error: err.message })
-      });
-    });
-  });
-};
-Valide les modifications et attends le redéploiement 👍j'ai toujours erreur api  50008:47On va voir exactement ce qui se passe. Sur GitHub, remplace le contenu de netlify/functions/orders.js par cette version de débogage :
-javascriptconst https = require('https');
-
-exports.handler = async function(event) {
   const token = event.headers['x-token'] || 'NO_TOKEN';
   const SHOP_ID = 339;
   
